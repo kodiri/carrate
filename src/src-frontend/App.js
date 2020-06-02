9a2ed1638acf82ee71722f7078350e19338f24c2
laurenpane <59893967+laurenpane@users.noreply.github.com>
2020-06-02 16:42:31 +0100

    Componentised elements of the application (#1)
    * Installed node modules
    
    * Installed react-router-dom
    
    * Componentised homepage,
    searchbar and navbar
    
    * Moved app.js and app.css into
    src-frontend and updated
    reference in index.js

---
+++ src/src-frontend/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar.js";
+import Homepage from "./Pages/Homepage/Homepage.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Homepage />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
