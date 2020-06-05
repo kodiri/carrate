fd5a95d8668d855f8b0b04c2ebc43b886be4bb9c
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-06-05 13:42:47 +0100

    pulled navbar changes

--- src/src-frontend/Pages/Homepage/Homepage.js
+++ src/src-frontend/Pages/Homepage/Homepage.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./Homepage.css";
-import Searchbar from "../../Components/Searchbar/Searchbar.js";
+// import Searchbar from "../../Components/Searchbar/Searchbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 export default function Homepage() {
   return (
-    <div>
+    <>
       <Router>
-        <div>Welcome to Carrate!</div>
-        <Searchbar />
+        <div className="homepage-container">
+          <h1 className="homepage-title">Carrate</h1>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap"
+            rel="stylesheet"
+          ></link>
+          <div className="homepage-searchbar"></div>
+          <div className="homepage-button-container">
+            <button className="homepage-button">Categories</button>
+            <button className="homepage-button">Filter</button>
+            <button className="homepage-button">Inspire Me</button>
+            <button className="homepage-button">Share</button>
+          </div>
+        </div>
+        {/* <Searchbar /> */}
       </Router>
-    </div>
+    </>
   );
 }
