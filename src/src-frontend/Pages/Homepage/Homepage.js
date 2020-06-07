1c7c80595cbf10c5c074998261c9f73034929eec
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-06-07 14:43:45 +0100

    Added inspire button, filter button

--- src/src-frontend/Pages/Homepage/Homepage.js
+++ src/src-frontend/Pages/Homepage/Homepage.js
@@ -1,27 +1,22 @@
 import React from "react";
-import "./Homepage.css";
-// import Searchbar from "../../Components/Searchbar/Searchbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import "./Homepage.css";
+import Searchbar from "../../Components/Searchbar/Searchbar.js";
+import InspireButton from "../../Components/InspireButton/InspireButton.js";
 
 export default function Homepage() {
   return (
     <>
       <Router>
         <div className="homepage-container">
-          <h1 className="homepage-title">Carrate</h1>
           <link
-            href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
             rel="stylesheet"
           ></link>
-          <div className="homepage-searchbar"></div>
-          <div className="homepage-button-container">
-            <button className="homepage-button">Categories</button>
-            <button className="homepage-button">Filter</button>
-            <button className="homepage-button">Inspire Me</button>
-            <button className="homepage-button">Share</button>
-          </div>
+          <Searchbar />
+          <InspireButton />
         </div>
-        {/* <Searchbar /> */}
       </Router>
     </>
   );
