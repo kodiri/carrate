33d2916bb28f800cdccd481617f8a91ae3d8f37e
Max Botez <xmax8888@gmail.com>
2020-06-11 13:56:57 +0100

    attached API to the app

--- src/src-frontend/Components/Searchbar/Searchbar.js
+++ src/src-frontend/Components/Searchbar/Searchbar.js
@@ -1,10 +1,71 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Searchbar.css";
+import Recipe from '../Navbar/Recipe/Recipe.js'
 
 import magnifierIcon from "../../Images/magnifier-icon.svg";
 import filterIcon from "../../Images/filter-icon.svg";
 
+const APP = () => {
+
+    const APP_ID = "948e2ff1";
+    const APP_KEY = "2d7de9309cad9acbecd59a9acaa0b598";
+
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('banana');
+
+    useEffect(() => {
+        getRecipes();
+    }, [query]);
+
+
+    const getRecipes = async () => {
+        const responce = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+
+        const data = await responce.json();
+        setRecipes(data.hits)
+        console.log(data.hits);
+    }
+    const updateSearch = e => {
+        setSearch(e.target.value);
+        // console.log(search);
+    }
+
+    const getSearch = e => {
+        e.preventDefault();
+        setQuery(search);
+        // setSearch(" ");  to empty the search bar after we pressed the submit button
+    }
+
+    return (
+        <div className="APP">
+            <form onSubmit={getSearch} className="searchbar" >
+                <input
+                    className="searchbar-text"
+                    type="text"
+                    value={search}
+                    onChange={updateSearch}
+                    // name="search"
+                    placeholder="type here"
+                     />
+                <button className="search-btn" type="submit">Search</button>
+            </form>
+            {recipes.map(recipe => (
+                <Recipe 
+                key={recipe.recipe.id}
+                title={recipe.recipe.label} 
+                calories={recipe.recipe.calories} 
+                image={recipe.recipe.image }
+                />
+            ))}
+        </div>
+    )
+}
+
+export default APP;
+
+/*
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
@@ -17,7 +78,9 @@ export default class SearchBar extends Component {
       search: event.target.value,
     });
   };
-  /* needs to be tied to the desired API
+*/
+
+/* needs to be tied to the desired API
 
 handleSubmit = (event) => {
   event.preventDefault();
@@ -25,6 +88,7 @@ handleSubmit = (event) => {
 }
 */
 
+/*
   render() {
     return (
       <div className="searchbar">
@@ -49,3 +113,4 @@ handleSubmit = (event) => {
     );
   }
 }
+*/
