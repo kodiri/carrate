b8bff279b052509161b86223c3afebc669e91def
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-06-11 16:54:53 +0100

    Hero Again (#8)

--- src/src-frontend/Components/Searchbar/Searchbar.js
+++ src/src-frontend/Components/Searchbar/Searchbar.js
@@ -1,25 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./Searchbar.css";
 
-/*
-export default function Searchbar() {
-  return <div>Hello, searchbar!</div>;
-}
-*/
-export default class SearchBar extends Component {
+import magnifierIcon from "../../Images/magnifier-icon.svg";
+import filterIcon from "../../Images/filter-icon.svg";
 
-  constructor(props){
+export default class SearchBar extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-        search: ''
-    }
-}
-handleChange = (event) => {
-  this.setState({
-      search: event.target.value
-  })
-}
-/* needs to be tied to the desired API
+      search: "",
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      search: event.target.value,
+    });
+  };
+  /* needs to be tied to the desired API
 
 handleSubmit = (event) => {
   event.preventDefault();
@@ -27,17 +25,27 @@ handleSubmit = (event) => {
 }
 */
 
-  render (){
+  render() {
     return (
-      <div className='search'>
+      <div className="searchbar">
         <form onSubmit={this.handleSubmit}>
-              <input id='searchbar' type='text' name='search' 
-                  placeholder='Search my igredients' 
-                  value={this.state.search} onChange={this.handleChange}
-              />
-              <input type='submit' value='Search'/>
+          <Link className="filter-icon">
+            <img src={filterIcon} alt="Filter Icon" />
+          </Link>
+          <input
+            className="searchbar-text"
+            id="searchbar"
+            type="text"
+            name="search"
+            placeholder="What's in the cupboard?"
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+          <Link className="search-button" to="/results">
+            <img src={magnifierIcon} alt="Search Button" />
+          </Link>
         </form>
       </div>
-    )
+    );
   }
 }
