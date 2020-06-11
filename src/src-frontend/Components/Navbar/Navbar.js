4493552ff8eafd0dc7fceaed1ca6f6c2d7c08230
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-06-11 16:45:51 +0100

    Hero Again

--- src/src-frontend/Components/Navbar/Navbar.js
+++ src/src-frontend/Components/Navbar/Navbar.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import Menu from "./Menu/Menu.js";
+
+import carrateAvatar from "../../Images/carrate.jpg";
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <Link className="navbar-item" to="/">
-        Home
+        {/* <img
+          className="navbar-item carrate-avatar"
+          src={carrateAvatar}
+          alt="Carrate Avatar"
+        /> */}
       </Link>
-      <Menu />
+      <div className="navbar-item title"></div>
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Muli:wght@300&display=swap"
+        rel="stylesheet"
+      ></link>
     </div>
   );
 }
