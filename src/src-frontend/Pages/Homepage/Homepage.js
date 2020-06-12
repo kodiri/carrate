1429d4ae597dc5343fa8b9e38bf51cb0ce3c8885
laurenpane <59893967+laurenpane@users.noreply.github.com>
2020-06-12 13:27:03 +0100

    UI/UX changes - made scalable from mobile to desktop (#10)
    * Installed node modules
    
    * Installed react-router-dom
    
    * Componentised homepage,
    searchbar and navbar
    
    * Moved app.js and app.css into
    src-frontend and updated
    reference in index.js
    
    * Started navbar and menubar
    
    * Added InspireMeButton
    
    * Searchbar changes
    
    * Searchbar
    
    * Removed reference to Navbar to correct positioning
    
    * Made scalable

--- src/src-frontend/Pages/Homepage/Homepage.js
+++ src/src-frontend/Pages/Homepage/Homepage.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./Homepage.css";
 import Searchbar from "../../Components/Searchbar/Searchbar.js";
 import InspireButton from "../../Components/InspireButton/InspireButton.js";
+import Hero from "../../Components/Hero/Hero.js";
 
 export default function Homepage() {
   return (
@@ -14,6 +15,7 @@ export default function Homepage() {
             href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
             rel="stylesheet"
           ></link>
+          <Hero />
           <Searchbar />
           <InspireButton />
         </div>
