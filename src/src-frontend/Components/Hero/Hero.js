7c518a093fb9fdd1ac73e1774ffef51b273232f9
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-06-12 07:07:07 +0100

    merged with master branch

--- src/src-frontend/Components/Hero/Hero.js
+++ src/src-frontend/Components/Hero/Hero.js
@@ -2,18 +2,6 @@ import React from "react";
 import "./Hero.css";
 import carrate from "./carrate.png";
 
-<<<<<<< HEAD
-const Hero = () => {
-  return (
-    <div className="wrapper">
-      <div className="logo-container">
-        <img src={carrate} alt={carrate} />
-      </div>
-      <p> Carrate</p>
-    </div>
-  );
-};
-=======
 
 function Hero () {
     return(
@@ -24,6 +12,5 @@ function Hero () {
         </div>
     )
 }
->>>>>>> b8bff279b052509161b86223c3afebc669e91def
 
 export default Hero;
--- src/src-frontend/Components/Navbar/Navbar.js
+++ src/src-frontend/Components/Navbar/Navbar.js
@@ -2,29 +2,11 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-import carrateAvatar from "../../Images/carrate.jpg";
-
 export default function Navbar() {
   return (
     <div className="navbar">
-      <Link className="navbar-item" to="/">
-<<<<<<< HEAD
-        <img
-          className="navbar-item carrate-avatar"
-          src={carrateAvatar}
-          alt="Carrate Avatar"
-        />
-      </Link>
-      <div className="navbar-item title">carrate</div>
-=======
-        {/* <img
-          className="navbar-item carrate-avatar"
-          src={carrateAvatar}
-          alt="Carrate Avatar"
-        /> */}
-      </Link>
+      <Link className="navbar-item" to="/"></Link>
       <div className="navbar-item title"></div>
->>>>>>> b8bff279b052509161b86223c3afebc669e91def
 
       <link
         href="https://fonts.googleapis.com/css2?family=Muli:wght@300&display=swap"
