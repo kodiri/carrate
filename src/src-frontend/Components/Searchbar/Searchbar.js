e7218845f146780cc2476e9ad9277a9ce5e6b45b
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-06-12 13:41:58 +0100

    received API

--- src/src-frontend/Components/Searchbar/Searchbar.js
+++ src/src-frontend/Components/Searchbar/Searchbar.js
@@ -1,66 +1,67 @@
 import React, { Component, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import "./Searchbar.css";
-import Recipe from '../Navbar/Recipe/Recipe.js'
+import Recipe from "../Navbar/Recipe/Recipe.js";
 
 import magnifierIcon from "../../Images/magnifier-icon.svg";
 import filterIcon from "../../Images/filter-icon.svg";
 
 const APP = () => {
+  const APP_ID = "948e2ff1";
+  const APP_KEY = "2d7de9309cad9acbecd59a9acaa0b598";
 
-    const APP_ID = "948e2ff1";
-    const APP_KEY = "2d7de9309cad9acbecd59a9acaa0b598";
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
 
-    const [recipes, setRecipes] = useState([]);
-    const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('');
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
-    useEffect(() => {
-        getRecipes();
-    }, [query]);
+  const getRecipes = async () => {
+    const responce = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
 
+    const data = await responce.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    // console.log(search);
+  };
 
-    const getRecipes = async () => {
-        const responce = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    // setSearch(" ");  to empty the search bar after we pressed the submit button
+  };
 
-        const data = await responce.json();
-        setRecipes(data.hits)
-        console.log(data.hits);
-    }
-    const updateSearch = e => {
-        setSearch(e.target.value);
-        // console.log(search);
-    }
-
-    const getSearch = e => {
-        e.preventDefault();
-        setQuery(search);
-        // setSearch(" ");  to empty the search bar after we pressed the submit button
-    }
-
-    return (
-        <div className="APP">
-            <form onSubmit={getSearch} className="searchbar" >
-                <input
-                    className="searchbar-text"
-                    type="text"
-                    value={search}
-                    onChange={updateSearch}
-                    // name="search"
-                    placeholder="type here"
-                     />
-                <button className="search-btn" type="submit">Search</button>
-            </form>
-            {recipes.map(recipe => (
-                <Recipe 
-                key={recipe.recipe.id}
-                title={recipe.recipe.label} 
-                calories={recipe.recipe.calories} 
-                image={recipe.recipe.image }
-                />
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="APP">
+      <form onSubmit={getSearch} className="searchbar">
+        <input
+          className="searchbar-text"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+          // name="search"
+          placeholder="type here"
+        />
+        <button className="search-btn" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.id}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default APP;
