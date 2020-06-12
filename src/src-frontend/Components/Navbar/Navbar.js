5f057e32e0de34123efb764c51f6d7ac1e038830
Max De Burnay-Bastos <48160441+mdeburnay@users.noreply.github.com>
2020-06-12 15:41:48 +0100

    Added filter button and changed search button (#13)
    * pulled navbar changes
    
    * Added inspire button, filter button
    
    * removed favicon and default png's
    
    * merged with master branch
    
    * received API
    
    * Altered search button and added filter/menu button
    
    Co-authored-by: MdBB <48160441+maximoose773@users.noreply.github.com>

--- src/src-frontend/Components/Navbar/Navbar.js
+++ src/src-frontend/Components/Navbar/Navbar.js
@@ -2,18 +2,10 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-import carrateAvatar from "../../Images/carrate.jpg";
-
 export default function Navbar() {
   return (
     <div className="navbar">
-      <Link className="navbar-item" to="/">
-        {/* <img
-          className="navbar-item carrate-avatar"
-          src={carrateAvatar}
-          alt="Carrate Avatar"
-        /> */}
-      </Link>
+      <Link className="navbar-item" to="/"></Link>
       <div className="navbar-item title"></div>
 
       <link
