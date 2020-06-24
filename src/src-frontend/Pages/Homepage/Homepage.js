c93eac271013a772f9af76fca207c3f255c10d94
laurenpane <59893967+laurenpane@users.noreply.github.com>
2020-06-24 14:12:43 +0100

    Added description/tagline  (#27)
    * Made recipe card page mobile friendly, and changed View
    Recipe to Back to Home
    
    * Random recipe now mobile friendly
    
    * Styled and properly linked 'Not Found'
    
    * Added brief description of Carrate

--- src/src-frontend/Pages/Homepage/Homepage.js
+++ src/src-frontend/Pages/Homepage/Homepage.js
@@ -5,7 +5,7 @@ import "./Homepage.css";
 import Searchbar from "../../Components/Searchbar/Searchbar.js";
 import InspireButton from "../../Components/InspireButton/InspireButton.js";
 import Hero from "../../Components/Hero/Hero.js";
-import Team from '../../Components/Team/Team.js'
+import Team from "../../Components/Team/Team.js";
 
 export default function Homepage() {
   return (
