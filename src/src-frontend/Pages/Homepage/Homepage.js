72e06015f10ca7c37b5b627433bdf27c8d9ed56b
Max Botez <xmax8888@gmail.com>
2020-06-25 00:40:40 +0100

    added team files

--- src/src-frontend/Pages/Homepage/Homepage.js
+++ src/src-frontend/Pages/Homepage/Homepage.js
@@ -5,26 +5,11 @@ import "./Homepage.css";
 import Searchbar from "../../Components/Searchbar/Searchbar.js";
 import InspireButton from "../../Components/InspireButton/InspireButton.js";
 import Hero from "../../Components/Hero/Hero.js";
-<<<<<<< HEAD
-import Team from '../../Components/Team/Team.js'
-=======
 import Team from "../../Components/Team/Team.js";
->>>>>>> upstream/master
 
 export default function Homepage() {
   return (
     <>
-<<<<<<< HEAD
-      <div className="homepage-container">
-        <link
-          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
-          rel="stylesheet"
-        ></link>
-        <Hero />
-        <Searchbar />
-        <InspireButton />
-        <Team />
-=======
       <div className="homepage">
         <div className="homepage-container">
           <link
@@ -35,7 +20,6 @@ export default function Homepage() {
           <Searchbar />
           <InspireButton />
         </div>
->>>>>>> upstream/master
       </div>
     </>
   );
